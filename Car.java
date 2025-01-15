94a6cb1028102515a0d5bee3c0c2a05c121a84db
Katherine Mcneil <w0417834@campus.nscc.ca>
2025-01-15 11:07:26 -0400

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,37 @@
+//KatherineMcNeil
+//January 15 2025
+//W0417834
+
+public class Car {
+// add attributes
+private String name;
+private int  model;
+private  String color;
+
+
+
+// add behaviour  getters and setters (methods)
+    //getters
+    public String getName () {
+       return name; 
+    }
+
+    public int getModel () {
+       return model; 
+    }
+    public String getColor () {
+       return color; 
+    }
+
+
+    // setters
+   public void setName (String n) {
+       this.name = n; 
+    }
+   public void setModel (int m) {
+      this.model = m; 
+    } 
+public void setColor (String c) {
+        this.color = c;
+    }  
+}
---
+++ Main.java
@@ -0,0 +1,23 @@
+//KatherineMcNeil
+//January 15 2025
+//W0417834
+public class Main {
+    public static void main(String[] args) {
+System.out.println("All is ok!" );
+
+    Car carOne = new Car();
+    System.out.println(carOne.getName () );
+
+    carOne.setName("Nissan");
+    System.out.println(carOne.getName () );
+
+    carOne.setName("KIA")  ;
+    System.out.println(carOne.getName ());
+
+    carOne.setModel(2020)  ;
+    System.out.println(carOne.getModel () );
+
+    carOne.setColor ("red")  ;
+    System.out.println(carOne.getColor () );
+    }
+}
